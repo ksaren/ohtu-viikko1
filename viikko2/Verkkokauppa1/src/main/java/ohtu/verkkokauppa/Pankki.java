fe0ec51eb9ca5f235d2ffb1365a6306c180914c8
Kaisa Sarén <kaisa.saren@gmail.com>
2017-03-24 15:37:09 +0200

    muutettu tiedostorakennetta, ehkä sotku...

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -0,0 +1,24 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pankki implements PankkiRajapinta {
+
+    private KirjanpitoRajapinta kirjanpito;
+
+    @Autowired
+    public Pankki(KirjanpitoRajapinta kp) {
+        kirjanpito = kp;
+    }
+
+    @Override
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tililta + " tilille " + tilille
+                + " viite " + viitenumero + " summa " + summa + "e");
+
+        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
+        return true;
+    }
+}
